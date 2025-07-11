b7b48ca414454a8bcaf8ac9e429e3f5e6a849e17
ClaraTeoMorais <clara.teodosio@escolar.ifrn.edu.br>
2025-07-10 22:11:13 -0300

    finalização da atividade - proj django

--- aula_07-04/AulasBD_v2/app/views.py
+++ aula_07-04/AulasBD_v2/app/views.py
@@ -96,7 +96,7 @@ def exercicio_2(request):
 
 def exercicio_3(request):
     # define a página HTML (template) que deverá será carregada
-    template = 'exercicio_2.html'
+    template = 'exercicio_3.html'
     try:
         # obtem a conexao com o BD
         conexao = obter_conexao()
@@ -107,14 +107,57 @@ def exercicio_3(request):
         # define o comando SQL que será executado
         sql = '''
             SELECT  e.nome as 'estado',
-                    c.nome, 
-                    b.nome
-
-            FROM Cidade c
-            FROM Bairro b
+                    c.nome as 'cidade', 
+                    b.nome 
+            
             INNER JOIN Turma t ON t.id = a.turma_id
 
-            ORDER BY t.nome, a.nome
+            FROM Bairro b
+            INNER JOIN Cidade c ON c.id = b.cidade_id
+            INNER JOIN Etsado e ON e.id = c.estado_id
+
+            ORDER BY e.nome, c.nome, b.nome
+        '''
+        
+        # usa o cursor para executar o SQL
+        cursor.execute(sql)
+        # obtem todos os registros retornados
+        registros = cursor.fetchall()
+
+        # define a pagina a ser carregada, adicionando os registros das tabelas 
+        return render(request, template, context={'registros': registros})
+    
+    # se ocorreu algunm erro, insere a mensagem para ser exibida no contexto da página 
+    except Exception as err:
+        return render(request, template, context={'ERRO': err})
+
+
+def exercicio_4(request):
+    # define a página HTML (template) que deverá será carregada
+    template = 'exercicio_4.html'
+    try:
+        # obtem a conexao com o BD
+        conexao = obter_conexao()
+
+        # define um cursor para executar comandos SQL
+        cursor = conexao.cursor()
+
+        # define o comando SQL que será executado
+        sql = '''
+            SELECT  f.descricao as 'fabricante',
+                    m.descricao as 'modelo', 
+                    c.descricao as 'categoria',
+                    carro.cor,
+                    carro.placa,
+                    carro.ano_fabricacao,
+                    carro.preco
+
+            FROM Carro carro
+            INNER JOIN Modelo m ON m.id = carro.modelo_id
+            INNER JOIN Categoria c ON m.id = c.categoria_id
+            INNER JOIN Fabricante f ON f.id = m.fabricante_id
+
+            ORDER BY f.descricao, m.descricao, carro.ano_fabricacao, carro.cor, carro.placa, carro.preco, c.descricao 
         '''
         
         # usa o cursor para executar o SQL
